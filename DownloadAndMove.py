c1ce07e3b95d933461b43226c650297b48aa399f
mihael079 <91776070+mihael079@users.noreply.github.com>
2024-01-30 12:27:56 +0800

    Add files via upload

---
+++ DownloadAndMove.py
@@ -0,0 +1,57 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# from_dir = "ENTER THE PATH OF DOWNLOAD FOLDER (USE " / ") in VSC"
+# to_dir = "ENTER THE PATH OF DESTINATION FOLDER(USE " / ") in VSC"
+
+from_dir = "/Users/mihael/Downloads"
+to_dir = "/Users/mihael/Documents/Python/CLASS 103"
+
+dir_tree = {
+    "Image_Files": ['.jpg', '.jpeg', '.png', '.gif', '.jfif'],
+    "Video_Files": ['.mpg', '.mp2', '.mpeg', '.mpe', '.mpv', '.mp4', '.m4p', '.m4v', '.avi', '.mov'],
+    "Document_Files": ['.ppt', '.xls', '.csv', '.pdf', '.txt'],
+    "Setup_Files": ['.exe', '.bin', '.cmd', '.msi', '.dmg']
+}
+
+# Event Hanlder Class
+
+class FileEventHandler(FileSystemEventHandler):
+
+  def on_created(self, event):
+     print(f"hey,{event.src_path} has been created!")
+  def on_deleted(self, event):
+     print(f"Oops ,someone deleted{event.src_path} !")
+
+
+
+# Initialize Event Handler Class
+event_handler = FileEventHandler()
+
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+try:
+   while True:
+      time.sleep(2)
+      print("running...")
+except KeyboardInterrupt:
+   print("stopped!")
+   observer.stop()
+    
